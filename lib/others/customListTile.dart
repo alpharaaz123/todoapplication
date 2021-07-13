37733d8febae20fe584bc0b0420f332f79d1b7a5
alpharaaz123 <raazbogati3@gmail.com>
2021-07-13 10:17:53 +0545

    Incomplete api fetching

--- lib/others/customListTile.dart
+++ lib/others/customListTile.dart
@@ -1,55 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:todoapplication/model/article_model.dart';
 
-    Widget customListTile(Article article) {
-    return Container(
-            margin: EdgeInsets.all(12.0),
-            padding: EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12.0),
-                boxShadow: [
-                BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 3.0,
-                ),
-                ]),
-            child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-                Container(
-                height: 200.0,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: NetworkImage(article.urlToImage), fit: BoxFit.cover),
-                    borderRadius: BorderRadius.circular(12.0),
-                ),
-                ),
-                SizedBox(height: 8.0),
-                Container(
-                padding: EdgeInsets.all(6.0),
-                decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(30.0),
-                ),
-                child: Text(
-                    article.source.name,
-                    style: TextStyle(
-                    color: Colors.white,
-                    ),
-                ),
-                ),
-                SizedBox(height: 8.0),
-                Text(
-                article.title,
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16.0,
-                ),
-                )
-            ],
+Widget customListTile(Article article) {
+  return InkWell(
+    onTap: () {
+       
+    },
+    child: Container(
+      margin: EdgeInsets.all(12.0),
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 3.0,
             ),
-        );
+          ]),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // ignore: unnecessary_null_comparison
+          article.urlToImage != null
+              ? Container(
+                  height: 200.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(article.urlToImage),
+                        fit: BoxFit.cover),
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                )
+              : Container(
+                  height: 200.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            'https://source.unsplash.com/weekly?coding'),
+                        fit: BoxFit.cover),
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                ),
+          SizedBox(height: 8.0),
+          Container(
+            padding: EdgeInsets.all(6.0),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            child: Text(
+              article.source.name,
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            article.title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16.0,
+            ),
+          )
+        ],
+      ),
+    ),
+  );
 }
